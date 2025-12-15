25a0005529ad318e8798c47b6cfb3f42ed470a59
Cody Tran <147000920+pastewarecode@users.noreply.github.com>
2025-12-14 17:24:26 -0700

    Updated function_app/__init__.py file
    Now the main function no longer calls run_analysis() to send data. Instead it obtains cached data from the blob storage container and read it.

--- function_app/__init__.py
+++ function_app/__init__.py
@@ -1,17 +1,28 @@
 import azure.functions as func
-from data_analysis import run_analysis
+from azure.storage.blob import BlobServiceClient
+import os
 import json
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+# UPDATED FUNCTION: No longer calls "run_analysis()", now only reads cached data.
+def main(req: func.HttpRequest) -> func.HttpResponse: 
     try:
-        summary = run_analysis()
+        #Variables
+        connection_string = os.environ["AzureWebJobsStorage"]
+        blob_storage = BlobServiceClient.from_connection_string(connection_string)
+
+        container = blob_storage.get_container_client("outputs")
+        blob = container.download_blob("processed_data_with_metrics.csv")
+        data = blob.readall().decode("utf-8")
+
         return func.HttpResponse(
-            json.dumps(summary),
-            status_code=200,
-            mimetype="application/json"
+            data, 
+            mimetype = "text/csv",
+            status_code = 200
         )
+    
     except Exception as e:
         return func.HttpResponse(
             f"Error: {str(e)}",
-            status_code=500
+            status_code = 500
         )
+
